2d29fe246f093f0b68cd03acfa53aba26799ce86
Nick <wotk8050@gmail.com>
2020-12-21 11:48:22 -0500

    Version 0.1.1
    Added documentation to the script and added a new message

--- botsy.py
+++ botsy.py
@@ -1,33 +1,43 @@
-import os
-import facebook
+import os ## import curent os
+import facebook ## imports the facebook sdk
 import random
-import swapi
-from PIL import Image, ImageDraw
+import swapi ## imports the star wars api
+from PIL import Image, ImageDraw ## imports pillow
 
-token = "your token here :P"
+token = "your token here :p"
 
 def pes():
 	rs = random.randint(1,82)
 	return rs
+	## Ranomizer component to get random number between 1 and 82
 
 
-SW_id = swapi.get_person(pes())
+SW_id = swapi.get_person(pes()) ## calls swapi to get person using randomizer above
 
-SW_more = SW_id.get_homeworld()
+SW_more = SW_id.get_homeworld() ## gets chosen persons homeworld
 
-SW_data = SW_id.get_vehicles()
+SW_data = SW_id.get_vehicles() ## gets number of chosen persons driven vehicles
 
-SW_vec = SW_data.items
+SW_space = SW_id.get_starships() ## gets number of chosen persons flown spaceships
 
-species = SW_id.get_species()
-spe = species.items
+species = SW_id.get_species() ## gets chosen persons species
 
-ld = str(SW_id)
+
+spe = species.items ## lists their species
+
+SW_space2 = SW_space.items ## lists their flown starships
+
+SW_vec = SW_data.items ## lists drivin vehicles
+
+ld = str(SW_id) ## each item beloow this converts them into a string using str
 ls = str(SW_more)
 ls2 = str(spe)
 ls3 = str(SW_vec)
+ls4 = str(SW_space2)
+
+## this function is the image generation, generates the image based on variables listed above
 def imagegen():
-	if(ls3 == "" or ls3 =="[]" and ls2 == "" or ls2 == "[]"):
+	if(ls3 == "" or ls3 =="[]" and ls2 == "" or ls2 == "[]"): ## if there are no vehicles and no species listed do this
 		print("ERROR 21: NO VEHICLE FOUND")
 		print("ERROR 22: NO SPECIES")
 		img = Image.new('RGB', (400, 400), color = (73, 109, 137))
@@ -39,7 +49,7 @@ def imagegen():
 		img.save('pil_text.png')
 		print(SW_id)
 		print(SW_more)
-	elif(ls3 == "" or ls3 == "[]"):
+	elif(ls3 == "" or ls3 == "[]"): ## if no vehicles but there is species do this
 		print("ERROR 21: NO VEHICLE FOUND")
 		img = Image.new('RGB', (400, 400), color = (73, 109, 137))
 		d = ImageDraw.Draw(img)
@@ -50,7 +60,7 @@ def imagegen():
 		img.save('pil_text.png')
 
 
-	else:
+	else: ## if both do this
 		img = Image.new('RGB', (400, 400), color = (73, 109, 137))
 		d = ImageDraw.Draw(img)
 		d.text((10,10), ld, fill=(255,255,0))
@@ -59,11 +69,11 @@ def imagegen():
 		d.text((10,90), ls3, fill=(255,255,0))
 		img.save('pil_text.png')
 
-imagegen()
+imagegen() ## make the image
 
-def postToFacebook(token,message="standby"):
+def postToFacebook(token,message="standby"): ## posts to facebook
 	graph = facebook.GraphAPI(token)
 
-	post_id = graph.put_photo(image = open('pil_text.png', 'rb'), message = SW_id )["post_id"]
+	post_id = graph.put_photo(image = open('pil_text.png', 'rb'), message = "Star Wars Person Of The Day: " )["post_id"]
 
 postToFacebook(token,message="standby")
